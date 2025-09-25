6abdfe0cb3989e1332f8c2642cd5d32bf8e1c4e6
fairuzulum <usleunmdi32@gmail.com>
2025-09-25 13:05:50 +0700

    variable-typesdata-operator

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,57 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+
+    // === 1. Variables & Data Types ===
+    int age = 25; // integer
+    double height = 170.5; // floating point
+    char grade = 'A'; // single character
+    boolean isStudent = true; // true or false
+    String name = "Fairuz"; // text (bukan primitif, tapi sering dipakai)
+
+    System.out.println("=== Variables & Data Types ===");
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Height: " + height + " cm");
+    System.out.println("Grade: " + grade);
+    System.out.println("Is Student? " + isStudent);
+
+    // === 2. Operators ===
+    int a = 10, b = 3;
+
+    // Arithmetic
+    System.out.println("\n=== Arithmetic Operators ===");
+    System.out.println("a + b = " + (a + b));
+    System.out.println("a - b = " + (a - b));
+    System.out.println("a * b = " + (a * b));
+    System.out.println("a / b = " + (a / b));
+    System.out.println("a % b = " + (a % b));
+
+    // Comparison
+    System.out.println("\n=== Comparison Operators ===");
+    System.out.println("a == b ? " + (a == b));
+    System.out.println("a != b ? " + (a != b));
+    System.out.println("a > b ? " + (a > b));
+    System.out.println("a < b ? " + (a < b));
+
+    // Logical
+    System.out.println("\n=== Logical Operators ===");
+    boolean x = true, y = false;
+    System.out.println("x && y = " + (x && y)); // AND
+    System.out.println("x || y = " + (x || y)); // OR
+    System.out.println("!x = " + (!x)); // NOT
+
+    // === 3. Type Casting ===
+    System.out.println("\n=== Type Casting ===");
+    double d = 9.78;
+    int i = (int) d; // explicit casting: double -> int
+    System.out.println("Double value: " + d);
+    System.out.println("After casting to int: " + i);
+
+    int num = 100;
+    double numDouble = num; // implicit casting: int -> double
+    System.out.println("Int value: " + num);
+    System.out.println("After casting to double: " + numDouble);
+
 }
